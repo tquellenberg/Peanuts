0335727ee192976a7269ab1434e339c225d94ee6
quelle <quelle@10.160.169.123>
2018-10-17 13:51:54 +0200

    Overlay icon in navigation if notes exist

--- de.tomsplayground.peanuts.client/src/main/java/de/tomsplayground/peanuts/client/navigation/NavigationLabelDecorator.java
+++ de.tomsplayground.peanuts.client/src/main/java/de/tomsplayground/peanuts/client/navigation/NavigationLabelDecorator.java
@@ -1,5 +1,6 @@
 package de.tomsplayground.peanuts.client.navigation;
 
+import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.jface.viewers.IDecoration;
 import org.eclipse.jface.viewers.ILabelProviderListener;
@@ -11,6 +12,7 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import de.tomsplayground.peanuts.client.app.Activator;
 import de.tomsplayground.peanuts.domain.base.IDeletable;
 import de.tomsplayground.peanuts.domain.base.Security;
+import de.tomsplayground.peanuts.domain.note.Note;
 import de.tomsplayground.peanuts.domain.statistics.SecurityCategoryMapping;
 
 public class NavigationLabelDecorator implements ILightweightLabelDecorator {
@@ -19,9 +21,12 @@ public class NavigationLabelDecorator implements ILightweightLabelDecorator {
 
 	private static final ImageDescriptor WARNING_OVR;
 
+	private static final ImageDescriptor INFO_OVR;
+
 	static {
 		DELETED_OVR = AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons2/error_ovr.png");
 		WARNING_OVR = PlatformUI.getWorkbench().getSharedImages().getImageDescriptor(ISharedImages.IMG_DEC_FIELD_WARNING);
+		INFO_OVR = AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons2/contassist_ovr.png");
 	}
 
 	@Override
@@ -54,9 +59,21 @@ public class NavigationLabelDecorator implements ILightweightLabelDecorator {
 			if (emptyCategoryMapping(security)) {
 				decoration.addOverlay(WARNING_OVR, IDecoration.TOP_RIGHT);
 			}
+			if (isNoteNotEmpty(security)) {
+				decoration.addOverlay(INFO_OVR, IDecoration.TOP_LEFT);				
+			}
 		}
 	}
 
+	private boolean isNoteNotEmpty(Security security) {
+		for (Note note : security.getNotes()) {
+			if (StringUtils.isNotBlank(note.getText())) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	private boolean emptyCategoryMapping(Security security) {
 		for (SecurityCategoryMapping securityCategoryMapping : Activator.getDefault().getAccountManager().getSecurityCategoryMappings()) {
 			if (securityCategoryMapping.getAllSecurities().contains(security)) {
