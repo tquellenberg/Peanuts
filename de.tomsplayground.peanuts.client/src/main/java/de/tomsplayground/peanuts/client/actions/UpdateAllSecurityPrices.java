1d02500d43b27a17998fe6fd6f0a60dfb20138e7
Tom Quellenberg <tom.quellenberg@gmail.com>
2022-01-06 10:00:39 +0100

    Show alarm view after refresh of prices

--- de.tomsplayground.peanuts.client/src/main/java/de/tomsplayground/peanuts/client/actions/UpdateAllSecurityPrices.java
+++ de.tomsplayground.peanuts.client/src/main/java/de/tomsplayground/peanuts/client/actions/UpdateAllSecurityPrices.java
@@ -7,9 +7,14 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PartInitException;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 import com.google.common.collect.ImmutableList;
 
+import de.tomsplayground.peanuts.client.alarm.AlarmView;
 import de.tomsplayground.peanuts.client.app.Activator;
 import de.tomsplayground.peanuts.client.editors.security.properties.SecurityPropertyPage;
 import de.tomsplayground.peanuts.domain.base.Security;
@@ -25,47 +30,69 @@ public class UpdateAllSecurityPrices extends AbstractHandler {
 		Job refreshPricesJob = new Job("Refresh investment prices") {
 			@Override
 			protected IStatus run(IProgressMonitor monitor) {
-				try {
-					PriceProviderFactory priceProviderFactory = PriceProviderFactory.getInstance();
-					ImmutableList<Security> securities = Activator.getDefault().getAccountManager().getSecurities();
-					monitor.beginTask("Refresh investment prices", securities.size());
-					for (Security security : securities) {
-						if (! security.isDeleted()) {
-							monitor.subTask("Refreshing " + security.getName());
-							priceProviderFactory.refresh(security, Boolean.valueOf(
-								security.getConfigurationValue(SecurityPropertyPage.OVERRIDE_EXISTING_PRICE_DATA)).booleanValue());
-							Scraping scraping = new Scraping(security);
-							Price price = scraping.execute();
-							if (price != null) {
-								IPriceProvider priceProvider = priceProviderFactory.getPriceProvider(security);
-								priceProvider.setPrice(price);
-								priceProviderFactory.saveToLocal(security, priceProvider);
-							}
-						}
-						monitor.worked(1);
-						if (monitor.isCanceled()) {
-							return Status.CANCEL_STATUS;
-						}
-						throttle();
-					}
-				} finally {
-					monitor.done();
+				IStatus status = refreshPrices(monitor);
+				if (status == Status.OK_STATUS) {
+					HandlerUtil.getActiveShell(event).getDisplay()
+						.asyncExec(() -> checkAlarmView(event));
 				}
-				return Status.OK_STATUS;
-			}
-
-			private void throttle() {
-				try {
-					Thread.sleep(2000);
-				} catch (InterruptedException e) {
-					Thread.currentThread().interrupt();
-				}
+				return status;
 			}
 		};
 		refreshPricesJob.setUser(false);
 		refreshPricesJob.setSystem(false);
 		refreshPricesJob.schedule();
+		
 		return null;
+	}
+
+	private void checkAlarmView(ExecutionEvent event) {
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindow(event);
+		if (window != null) {
+			try {
+				IViewPart view = window.getActivePage().showView(AlarmView.ID);
+				view.setFocus();
+			} catch (PartInitException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private IStatus refreshPrices(IProgressMonitor monitor) {
+		try {
+			PriceProviderFactory priceProviderFactory = PriceProviderFactory.getInstance();
+			ImmutableList<Security> securities = Activator.getDefault().getAccountManager().getSecurities();
+			monitor.beginTask("Refresh investment prices", securities.size());
+			for (Security security : securities) {
+				if (! security.isDeleted()) {
+					monitor.subTask("Refreshing " + security.getName());
+					priceProviderFactory.refresh(security, Boolean.valueOf(
+						security.getConfigurationValue(SecurityPropertyPage.OVERRIDE_EXISTING_PRICE_DATA)).booleanValue());
+					Scraping scraping = new Scraping(security);
+					Price price = scraping.execute();
+					if (price != null) {
+						IPriceProvider priceProvider = priceProviderFactory.getPriceProvider(security);
+						priceProvider.setPrice(price);
+						priceProviderFactory.saveToLocal(security, priceProvider);
+					}
+				}
+				monitor.worked(1);
+				if (monitor.isCanceled()) {
+					return Status.CANCEL_STATUS;
+				}
+				throttle();
+			}
+		} finally {
+			monitor.done();
+		}
+		return Status.OK_STATUS;
+	}
+
+	private void throttle() {
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		}
 	}
 
 }
