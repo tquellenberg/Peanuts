35b100fbe20703f251c0ee3681babe86b1121fdc
Tom Quellenberg <tom.quellenberg@gmail.com>
2022-07-07 20:08:37 +0200

    yield is keyword

--- de.tomsplayground.peanuts/src/main/java/de/tomsplayground/peanuts/domain/statistics/Volatility.java
+++ de.tomsplayground.peanuts/src/main/java/de/tomsplayground/peanuts/domain/statistics/Volatility.java
@@ -23,8 +23,8 @@ public class Volatility {
 		}
 		Day minDate = maxDate.addYear(-1);
 		ImmutableList<BigDecimal> values = pp.getPrices(minDate, maxDate).stream()
-			.map(IPrice::getValue)
-			.collect(ImmutableList.toImmutableList());
+				.map(IPrice::getValue)
+				.collect(ImmutableList.toImmutableList());
 		if (values.size() < 3) {
 			return 0.0;
 		}
@@ -34,32 +34,32 @@ public class Volatility {
 	public double calculateVolatility(ImmutableList<BigDecimal> values) {
 		log.debug("Size: {}", values.size());
 		BigDecimal v1 = values.get(0);
-		double yield[] = new double[values.size()-1];
+		double yiel[] = new double[values.size() - 1];
 		int i = 0;
 		for (BigDecimal v2 : values.subList(1, values.size())) {
-			yield[i] = Math.log(v2.divide(v1, PeanutsUtil.MC).doubleValue());
+			yiel[i] = Math.log(v2.divide(v1, PeanutsUtil.MC).doubleValue());
 			if (log.isDebugEnabled()) {
-				log.debug("V1:"+v1+ " V2:"+v2+" Y:"+yield[i]);
+				log.debug("V1:" + v1 + " V2:" + v2 + " Y:" + yiel[i]);
 			}
 			i++;
 			v1 = v2;
 		}
 		double avg = 0;
-		for (double r : yield) {
+		for (double r : yiel) {
 			avg += r;
 		}
-		avg = avg / yield.length;
+		avg = avg / yiel.length;
 		log.debug("AVG: {}", avg);
 		double volatility = 0;
-		for (double r : yield) {
+		for (double r : yiel) {
 			double d = (r - avg);
-			volatility += (d*d);
+			volatility += (d * d);
 		}
 		log.debug("Vola1: {}", volatility);
 		if (volatility < 0.001) {
 			return 0.0;
 		}
-		volatility = Math.sqrt((volatility * 252) / (yield.length - 1));
+		volatility = Math.sqrt((volatility * 252) / (yiel.length - 1));
 		log.debug("Vola2: {}", volatility);
 		return volatility;
 	}
